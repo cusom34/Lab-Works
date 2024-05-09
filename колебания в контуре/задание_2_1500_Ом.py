6e78902bdccd2b6348e1b5efc12dba8cff53a5a2
Alex Cold <70763567+cusom34@users.noreply.github.com>
2024-05-09 17:19:07 +0300

    Add files via upload

---
+++ колебания в контуре/задание_2_1500_Ом.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import openpyxl
+from scipy.interpolate import PchipInterpolator
+
+path = "R1500_Om.xlsx"
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active
+
+data = []
+for i in range(1,4+1):
+    for j in range(2,51+1):
+        data +=[sheet_obj.cell(j, i).value]
+
+freq = range(0,6500)
+f = data[:50]
+UC = data[50:100]
+UL = data[100:150]
+UR = data[150:200]
+
+fig = plt.figure(figsize=(15, 4))
+plt.title('R = 1500 Ом')
+
+plt.scatter(f,UC, marker = '.', label='UC')
+UC_inter = PchipInterpolator(f, UC)
+plt.plot(freq,UC_inter(freq)+0.005)
+
+plt.scatter(f,UL, marker = '.', label='UL')
+UL_inter = PchipInterpolator(f, UL)
+plt.plot(freq,UL_inter(freq)+0.005)
+
+plt.scatter(f,UR, marker = '.', label='UR')
+UR_inter = PchipInterpolator(f, UR)
+plt.plot(freq,UR_inter(freq) + 0.015)
+
+plt.legend(loc='upper right')
+plt.ylabel("U, В")
+plt.xlabel("f, Гц")
+plt.xticks(range(0,6501,250))
+plt.grid()
+plt.show()
