bbf97faa83dd0aeb7e37f285d9599c113780990a
ChaosAroundY0u <152779643+ChaosAroundY0u@users.noreply.github.com>
2024-05-11 10:49:26 +0300

    Update задание_2_1500_Ом.py
    ыыы2

--- колебания в контуре/задание_2_1500_Ом.py
+++ колебания в контуре/задание_2_1500_Ом.py
@@ -1,6 +1,21 @@
 import matplotlib.pyplot as plt
 import openpyxl
 from scipy.interpolate import PchipInterpolator
+import numpy as np
+
+r = 1500 +250 + 365
+l = 0.39
+c = 5.7e-9
+E0= 0.248
+
+def UR_func(f):
+       return  E0*r/(r**2 + (2*np.pi*f*l-1/(2*np.pi*f*c))**2)**0.5
+
+def UC_func(f):
+    return E0/(2*np.pi*f*c*(r**2 + (2*np.pi*f*l-1/(2*np.pi*f*c))**2)**0.5)
+
+def UL_func(f):
+       return  E0*2*np.pi*f*l/((r**2 + (2*np.pi*f*l-1/(2*np.pi*f*c))**2)**0.5)
 
 path = "R1500_Om.xlsx"
 wb_obj = openpyxl.load_workbook(path)
@@ -17,20 +32,27 @@ UC = data[50:100]
 UL = data[100:150]
 UR = data[150:200]
 
+UR_new = [UR_func(elem) for elem in freq[1:]]
+UC_new = [UC_func(elem) for elem in freq[1:]]
+UL_new = [UL_func(elem) for elem in freq[1:]]
+
 fig = plt.figure(figsize=(15, 4))
 plt.title('R = 1500 Ом')
 
 plt.scatter(f,UC, marker = '.', label='UC')
 UC_inter = PchipInterpolator(f, UC)
-plt.plot(freq,UC_inter(freq)+0.005)
+#plt.plot(freq,UC_inter(freq)+0.005)
+plt.plot(freq[1:],UC_new)
 
 plt.scatter(f,UL, marker = '.', label='UL')
 UL_inter = PchipInterpolator(f, UL)
-plt.plot(freq,UL_inter(freq)+0.005)
+#plt.plot(freq,UL_inter(freq)+0.005)
+plt.plot(freq[1:],UL_new)
 
 plt.scatter(f,UR, marker = '.', label='UR')
 UR_inter = PchipInterpolator(f, UR)
-plt.plot(freq,UR_inter(freq) + 0.015)
+#plt.plot(freq,UR_inter(freq) + 0.015)
+plt.plot(freq[1:],UR_new)
 
 plt.legend(loc='upper right')
 plt.ylabel("U, В")
